133045acc2cc086993e46f64f9c9114e6c650346
nikhiljangid120 <nikhilverse58@gmail.com>
2025-12-28 17:41:28 +0530

    feat: add script to verify and display records from SQLite and NeDB databases

--- server/check-db.js
+++ server/check-db.js
@@ -3,6 +3,7 @@ const Datastore = require('nedb');
 const path = require('path');
 
 console.log('--- XYZ Backend Verification ---\n');
+
 const dataDir = path.join(__dirname, 'data');
 
 const dbPath = path.join(dataDir, 'contacts.sqlite');
