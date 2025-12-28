5ec0190902024bcfaa3bbfd75c6dfe95775e938d
nikhiljangid120 <nikhilverse58@gmail.com>
2025-12-28 17:36:57 +0530

    feat: Add script to verify and display last 5 records from SQLite and NeDB databases.

--- server/check-db.js
+++ server/check-db.js
@@ -1,7 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const Datastore = require('nedb');
 const path = require('path');
-
 console.log('--- XYZ Backend Verification ---\n');
 
 const dataDir = path.join(__dirname, 'data');
